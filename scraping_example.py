615c529bd5192d3b52f768c1e7f77846ebe04f37
fgshun <fgshun@gmail.com>
2019-12-28 23:10:31 +0900

    queue をメソッド引数からインスタンス属性へ移動

--- scraping_example.py
+++ scraping_example.py
@@ -26,26 +26,29 @@ class Error(Exception):
 
 
 class Downloader(abc.ABC):
-    concurrent: int
+    _concurrent: int
     download_queue: asyncio.Queue
     save_queue: asyncio.Queue
 
     def __init__(self, *, concurrent: int = 3, save_concurrent: int = 3) -> None:
-        self.concurrent = concurrent
-        self.save_concurrent = save_concurrent
+        self._concurrent = concurrent
+        self.download_queue: asyncio.Queue = asyncio.Queue()
+        self.save_queue: asyncio.Queue = asyncio.Queue(save_concurrent)
+
+    @property
+    def concurrent(self) -> int:
+        return self._concurrent
+
+    @property
+    def save_concurrent(self) -> int:
+        return self.save_queue.maxsize
 
     async def run(self) -> None:
-        num_download_task = self.concurrent
-        num_save_task = self.save_concurrent
+        self.prepare()
 
-        download_queue: asyncio.Queue = asyncio.Queue()
-        save_queue: asyncio.Queue = asyncio.Queue(num_save_task)
-
-        self.prepare(download_queue)
-
-        download_tasks = {asyncio.create_task(self.download(download_queue, save_queue)) for _ in range(num_download_task)}
-        save_tasks = {asyncio.create_task(self.save(save_queue)) for _ in range(num_save_task)}
-        download_queue_join = asyncio.create_task(download_queue.join())
+        download_tasks = {asyncio.create_task(self.download()) for _ in range(self.concurrent)}
+        save_tasks = {asyncio.create_task(self.save()) for _ in range(self.save_concurrent)}
+        download_queue_join = asyncio.create_task(self.download_queue.join())
 
         done, pending = await asyncio.wait(download_tasks | save_tasks | {download_queue_join}, return_when=asyncio.FIRST_COMPLETED)
         if done - {download_queue_join}:
@@ -54,7 +57,7 @@ class Downloader(abc.ABC):
             task.cancel()
         await asyncio.gather(*download_tasks, return_exceptions=True)
 
-        save_queue_join = asyncio.create_task(save_queue.join())
+        save_queue_join = asyncio.create_task(self.save_queue.join())
         done, pending = await asyncio.wait(save_tasks | {save_queue_join}, return_when=asyncio.FIRST_COMPLETED)
         if done - {save_queue_join}:
             raise Error('raise Exception for save method. stop run method.')
@@ -63,31 +66,31 @@ class Downloader(abc.ABC):
         await asyncio.gather(*save_tasks, return_exceptions=True)
 
     @abc.abstractmethod
-    def prepare(self, download_queue: asyncio.Queue) -> None:
+    def prepare(self) -> None:
         raise NotImplementedError
 
     @abc.abstractmethod
-    async def download(self, download_queue: asyncio.Queue, save_queue: asyncio.Queue) -> None:
+    async def download(self) -> None:
         async with aclosing(AsyncHTMLSession()) as session:
             while True:
-                url = await download_queue.get()
+                url = await self.download_queue.get()
 
                 logger.debug('download: %s', url)
                 res = await session.get(url, timeout=10.0)
                 res.raise_for_status()
-                await save_queue.put((url, res.content))
+                await self.save_queue.put((url, res.content))
                 logger.info('downloaded: %s', url)
 
-                download_queue.task_done()
+                self.download_queue.task_done()
                 await asyncio.sleep(0)
 
     @abc.abstractmethod
-    async def save(self, save_queue: asyncio.Queue) -> None:
+    async def save(self) -> None:
         while True:
-            url, data = await save_queue.get()
+            url, data = await self.save_queue.get()
             logger.info('%s (%d bytes)', url, len(data))
 
-            save_queue.task_done()
+            self.save_queue.task_done()
             await asyncio.sleep(0)
 
 
@@ -106,7 +109,7 @@ class ImgDownloader(Downloader):
         self.base_urls = base_urls
         self.dry_run = dry_run
 
-    def prepare(self, download_queue: asyncio.Queue) -> None:
+    def prepare(self) -> None:
         image_src_set = set()
         with closing(HTMLSession()) as session:
             for base_url in self.base_urls:
@@ -123,17 +126,17 @@ class ImgDownloader(Downloader):
                     image_src_set.add(src)
 
         for image_src in image_src_set:
-            download_queue.put_nowait(image_src)
+            self.download_queue.put_nowait(image_src)
 
-    async def download(self, download_queue: asyncio.Queue, save_queue: asyncio.Queue) -> None:
+    async def download(self) -> None:
         if self.dry_run:
             while True:
-                url = await download_queue.get()
+                url = await self.download_queue.get()
                 print(url)
-                download_queue.task_done()
+                self.download_queue.task_done()
                 await asyncio.sleep(0)
         else:
-            return await super().download(download_queue, save_queue)
+            return await super().download()
 
 
 class ImgFileDownloader(ImgDownloader):
@@ -148,9 +151,9 @@ class ImgFileDownloader(ImgDownloader):
 
         await super().run()
 
-    async def save(self, save_queue: asyncio.Queue) -> None:
+    async def save(self) -> None:
         while True:
-            url, data = await save_queue.get()
+            url, data = await self.save_queue.get()
             path = self._url2path(url)
 
             logger.debug('save: %s', path)
@@ -158,7 +161,7 @@ class ImgFileDownloader(ImgDownloader):
                 await f.write(data)
             logger.info('saved: %s', path)
 
-            save_queue.task_done()
+            self.save_queue.task_done()
             await asyncio.sleep(0)
 
 
@@ -188,17 +191,17 @@ class ImgSQLiteDownloader(ImgDownloader):
 
         await super().run()
 
-    async def save(self, save_queue: asyncio.Queue) -> None:
+    async def save(self) -> None:
         async with self.conn() as conn:
             while True:
-                url, data = await save_queue.get()
+                url, data = await self.save_queue.get()
 
                 logger.debug('save: %s', url)
                 await conn.execute('INSERT OR REPLACE INTO img (url, data) VALUES (?, ?)', (url, data))
                 await conn.commit()
                 logger.info('saved: %s', url)
 
-                save_queue.task_done()
+                self.save_queue.task_done()
                 await asyncio.sleep(0)
 
 
